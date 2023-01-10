84e926f1869d617b6feadd62effe0a81c4eb36fb
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-10 14:51:01 -0800

    update styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,7 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer>
-      <a href="https://www.flaticon.com/free-icons/pisces" title="pisces icons">
-        Zodiac icons created by Freepik - Flaticon
-      </a>
+      <span>Zodiac icons created by Freepik - Flaticon</span>
     </footer>
   );
 }
